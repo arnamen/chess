f69d45f3bc1aad44831f87cd7bb7f7961f6a1694
arnamen <aew11111d@gmail.com>
2021-02-28 20:41:50 +0200

    added knight moves

--- src/utils/moves-logic-helper.ts
+++ src/utils/moves-logic-helper.ts
@@ -62,7 +62,9 @@ export const getPossibleMoves = (chessboard: ChessPieceType[][], selectedPiece: 
         case 'BISHOP':
             possibleMoves = getPossibleMoves_BISHOP(chessboard, selectedPiece);
             break;
-
+        case 'KNIGHT':
+            possibleMoves = getPossibleMoves_KNIGHT(chessboard, selectedPiece);
+            break;
         default:
             break;
     }
@@ -259,6 +261,28 @@ const getPossibleMoves_BISHOP = (chessboard: ChessPieceType[][], selectedPiece: 
     if (movesLowerRight) possibleMoves.push(...movesLowerRight)
 
     console.log(possibleMoves)
+
+    return possibleMoves;
+}
+
+const getPossibleMoves_KNIGHT = (chessboard: ChessPieceType[][], selectedPiece: SelectedPiece): TileIndex[] => {
+
+    if (!selectedPiece || selectedPiece.piece.type !== 'KNIGHT') return [];
+
+    const possibleMoves: TileIndex[] = [];
+    const {x, y} = selectedPiece.tileIndex;
+    //up
+    if(chessboard[y-2] && chessboard[y-2][x-1] && chessboard[y-2][x-1].side !== selectedPiece.piece.side) possibleMoves.push({y: y-2, x: x-1});
+    if(chessboard[y-2] && chessboard[y-2][x+1] && chessboard[y-2][x+1].side !== selectedPiece.piece.side) possibleMoves.push({y: y-2, x: x+1});
+    //down
+    if(chessboard[y+2] && chessboard[y+2][x-1] && chessboard[y+2][x-1].side !== selectedPiece.piece.side) possibleMoves.push({y: y+2, x: x-1});
+    if(chessboard[y+2] && chessboard[y+2][x+1] && chessboard[y+2][x+1].side !== selectedPiece.piece.side) possibleMoves.push({y: y+2, x: x+1});
+    //left
+    if(chessboard[y+1] && chessboard[y+1][x-2] && chessboard[y+1][x-2].side !== selectedPiece.piece.side) possibleMoves.push({y: y+1, x: x-2});
+    if(chessboard[y-1] && chessboard[y-1][x-2] && chessboard[y-1][x-2].side !== selectedPiece.piece.side) possibleMoves.push({y: y-1, x: x-2});
+    //right
+    if(chessboard[y+1] && chessboard[y+1][x+2] && chessboard[y+1][x+2].side !== selectedPiece.piece.side) possibleMoves.push({y: y+1, x: x+2});
+    if(chessboard[y-1] && chessboard[y-1][x+2] && chessboard[y-1][x+2].side !== selectedPiece.piece.side) possibleMoves.push({y: y-1, x: x+2});
 
     return possibleMoves;
 }
